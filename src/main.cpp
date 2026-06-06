2dfbf0a028f38839d641821d363808d4419087ea
Abid <mabidmiah1998@gmail.com>
2026-06-07 00:32:08 +0100

    - Linked callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
-int main() { return 0; }
+#include "./book/order_book.hpp"
+#include "./feed/itch_parser.hpp"
+
+int main() {
+    book::OrderBook book;
+
+    itch::Callbacks cb;
+    cb.on_add_order     = [&](const itch::AddOrderMsg& m)      { book.add_order(m);     };
+    cb.on_order_cancel  = [&](const itch::OrderCancelMsg& m)   { book.cancel_order(m);  };
+    cb.on_order_delete  = [&](const itch::OrderDeleteMsg& m)   { book.delete_order(m);  };
+    cb.on_order_executed= [&](const itch::OrderExecutedMsg& m) { book.execute_order(m); };
+    cb.on_order_replace = [&](const itch::OrderReplaceMsg& m)  { book.replace_order(m); };
+
+    itch::Parser parser(std::move(cb));
+    return 0; 
+}
